3d3d6e4e627480597cb3374ac802a8dbb0d7d80d
YangHao <zjyh16@hotmail.com>
2015-02-17 00:24:43 +0800

    完成Ipv4的地址输入文本框
    Ipv4AddressTextBox

---
+++ IpAddressTextBox.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace IpTextBox
+{
+    partial class IpAddressTextBox
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Box4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Box3 = new System.Windows.Forms.TextBox();
+            this.Box2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Box1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Box4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Box3);
+            this.panel1.Controls.Add(this.Box2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Box1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.MinimumSize = new System.Drawing.Size(100, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(106, 18);
+            this.panel1.TabIndex = 0;
+            this.panel1.EnabledChanged += new System.EventHandler(this.panel1_EnabledChanged);
+            // 
+            // Box4
+            // 
+            this.Box4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Box4.Location = new System.Drawing.Point(83, 0);
+            this.Box4.MaxLength = 3;
+            this.Box4.Name = "Box4";
+            this.Box4.Size = new System.Drawing.Size(20, 14);
+            this.Box4.TabIndex = 4;
+            this.Box4.TabStop = false;
+            this.Box4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Box4.Enter += new System.EventHandler(this.Box_Enter);
+            this.Box4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Box4_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(21, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(8, 14);
+            this.label3.TabIndex = 6;
+            this.label3.Text = ".";
+            this.label3.EnabledChanged += new System.EventHandler(this.label_EnabledChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(77, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(8, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ".";
+            this.label2.EnabledChanged += new System.EventHandler(this.label_EnabledChanged);
+            // 
+            // Box3
+            // 
+            this.Box3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Box3.Location = new System.Drawing.Point(57, 0);
+            this.Box3.MaxLength = 3;
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(20, 14);
+            this.Box3.TabIndex = 3;
+            this.Box3.TabStop = false;
+            this.Box3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Box3.Enter += new System.EventHandler(this.Box_Enter);
+            this.Box3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Box3_KeyPress);
+            // 
+            // Box2
+            // 
+            this.Box2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Box2.Location = new System.Drawing.Point(29, 0);
+            this.Box2.MaxLength = 3;
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(20, 14);
+            this.Box2.TabIndex = 2;
+            this.Box2.TabStop = false;
+            this.Box2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Box2.Enter += new System.EventHandler(this.Box_Enter);
+            this.Box2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Box2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(49, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(8, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ".";
+            this.label1.EnabledChanged += new System.EventHandler(this.label_EnabledChanged);
+            // 
+            // Box1
+            // 
+            this.Box1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Box1.Location = new System.Drawing.Point(1, 0);
+            this.Box1.MaxLength = 3;
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(20, 14);
+            this.Box1.TabIndex = 1;
+            this.Box1.TabStop = false;
+            this.Box1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Box1.Enter += new System.EventHandler(this.Box_Enter);
+            this.Box1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Box1_KeyPress);
+            // 
+            // IpAddressTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(106, 18);
+            this.MinimumSize = new System.Drawing.Size(106, 18);
+            this.Name = "IpAddressTextBox";
+            this.Size = new System.Drawing.Size(106, 18);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        /// <summary>
+        /// The box1
+        /// </summary>
+        private System.Windows.Forms.TextBox Box1;
+        /// <summary>
+        /// The label1
+        /// </summary>
+        private System.Windows.Forms.Label label1;
+        /// <summary>
+        /// The box2
+        /// </summary>
+        private System.Windows.Forms.TextBox Box2;
+        /// <summary>
+        /// The box3
+        /// </summary>
+        private System.Windows.Forms.TextBox Box3;
+        //private System.Windows.Forms.TextBox Box4;
+        /// <summary>
+        /// The label2
+        /// </summary>
+        private System.Windows.Forms.Label label2;
+        /// <summary>
+        /// The label3
+        /// </summary>
+        private System.Windows.Forms.Label label3;
+        /// <summary>
+        /// The tool tip1
+        /// </summary>
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox Box4;
+    }
+}
---
+++ IpAddressTextBox.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IpTextBox
+{
+    [ToolboxBitmap(typeof(TextBox))]
+    public partial class IpAddressTextBox : UserControl
+    {
+        public IpAddressTextBox()
+        {
+            InitializeComponent();
+        }
+
+        /** Sets and Gets the tooltiptext on toolTip1 */
+        /// <summary>
+        /// Gets or sets the tool tip text.
+        /// </summary>
+        /// <value>
+        /// The tool tip text.
+        /// </value>
+        public string ToolTipText
+        {
+            get
+            { return this.toolTip1.GetToolTip(Box1); }
+            set
+            {
+                this.toolTip1.SetToolTip(Box1, value);
+                this.toolTip1.SetToolTip(Box2, value);
+                this.toolTip1.SetToolTip(Box3, value);
+                this.toolTip1.SetToolTip(Box4, value);
+                this.toolTip1.SetToolTip(label1, value);
+                this.toolTip1.SetToolTip(label2, value);
+                this.toolTip1.SetToolTip(label3, value);
+            }
+        }
+
+        /** Set or Get the string that represents the value in the box */
+        /// <summary>
+        /// </summary>
+        /// <returns>与该控件关联的文本。</returns>
+        public override string Text
+        {
+            get
+            {
+                return Box1.Text + "." + Box2.Text + "." + Box3.Text + "." + Box4.Text;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    string[] pieces = value.Split(".".ToCharArray(), 4);
+                    Box1.Text = pieces[0];
+                    Box2.Text = pieces[1];
+                    Box3.Text = pieces[2];
+                    Box4.Text = pieces[3];
+                }
+                else
+                {
+                    Box1.Text = "";
+                    Box2.Text = "";
+                    Box3.Text = "";
+                    Box4.Text = "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines whether this instance is valid.
+        /// </summary>
+        /// <returns>
+        ///   <see langword="true" /> if this instance is valid; otherwise, <see langword="false" />.
+        /// </returns>
+        public bool IsValid()
+        {
+            try
+            {
+                int checkval = int.Parse(Box1.Text);
+                if (checkval < 0 || checkval > 255)
+                    return false;
+                checkval = int.Parse(Box2.Text);
+                if (checkval < 0 || checkval > 255)
+                    return false;
+                checkval = int.Parse(Box3.Text);
+                if (checkval < 0 || checkval > 255)
+                    return false;
+                checkval = int.Parse(Box4.Text);
+                return checkval >= 0 && checkval <= 255;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the specified in string is valid.
+        /// </summary>
+        /// <param name="inString">The in string.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>
+        ///   <see langword="true" /> if the specified in string is valid; otherwise, <see langword="false" />.
+        /// </returns>
+        ///if not hide_events
+        ///Checks that a string passed in resolves to an integer value between 0 and 255
+        ///param inString The string passed in for testing
+        ///return True if the string is between 0 and 255 inclusively, false otherwise
+        ///endif
+        private bool IsValid(string inString, int x, int y)
+        {
+            try
+            {
+                int theValue = int.Parse(inString);
+                if (theValue >= x && theValue <= y) return true;
+                MessageBox.Show("输入值必须介于 0 与" + y + "之间", "超出可输入范围");
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// \ifnot hide_events
+        /// Performs KeyPress analysis and handling to ensure a valid ip octet is
+        /// being entered in Box1.
+        /// \endif
+        private void Box1_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            //Only Accept a '.', a numeral, or backspace
+            if (e.KeyChar.ToString() == "." || Char.IsDigit(e.KeyChar) || e.KeyChar == 8)
+            {
+                //If the key pressed is a '.'
+                if (e.KeyChar.ToString() == ".")
+                {
+                    //If the Text is a valid ip octet move to the next box
+                    if (Box1.Text != "" && Box1.Text.Length != Box1.SelectionLength)
+                    {
+                        if (IsValid(Box1.Text, 0, 255))
+                            Box2.Focus();
+                        else
+                            Box1.SelectAll();
+                    }
+                    e.Handled = true;
+                }
+
+                //If we are not overwriting the whole text
+                else if (Box1.SelectionLength != Box1.Text.Length)
+                {
+                    //Check that the new Text value will be a valid
+                    // ip octet then move on to next box
+                    if (Box1.Text.Length == 2)
+                    {
+                        if (e.KeyChar == 8)
+                            Box1.Text.Remove(Box1.Text.Length - 1, 1);
+                        else if (!IsValid(Box1.Text + e.KeyChar.ToString(), 0, 255))
+                        {
+                            Box1.SelectAll();
+                            e.Handled = true;
+                        }
+                        else
+                        {
+                            Box2.Focus();
+                        }
+                    }
+                }
+            }
+            //Do nothing if the keypress is not numeral, backspace, or '.'
+            else
+                e.Handled = true;
+        }
+
+        /// \ifnot hide_events
+        /// Performs KeyPress analysis and handling to ensure a valid ip octet is
+        /// being entered in Box2.
+        /// \endif
+        private void Box2_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            //Similar to Box1_KeyPress but in special case for backspace moves cursor
+            //to the previouse box (Box1)
+            if (e.KeyChar.ToString() == "." || Char.IsDigit(e.KeyChar) || e.KeyChar == 8)
+            {
+                if (e.KeyChar.ToString() == ".")
+                {
+                    if (Box2.Text != "" && Box2.Text.Length != Box2.SelectionLength)
+                    {
+                        if (IsValid(Box2.Text, 0, 255))
+                            Box3.Focus();
+                        else
+                            Box2.SelectAll();
+                    }
+                    e.Handled = true;
+                }
+                else if (Box2.SelectionLength != Box2.Text.Length)
+                {
+                    if (Box2.Text.Length == 2)
+                    {
+                        if (e.KeyChar == 8)
+                        {
+                            Box2.Text.Remove(Box2.Text.Length - 1, 1);
+                        }
+                        else if (!IsValid(Box2.Text + e.KeyChar.ToString(), 0, 255))
+                        {
+                            Box2.SelectAll();
+                            e.Handled = true;
+                        }
+                        else
+                        {
+                            Box3.Focus();
+                        }
+                    }
+                }
+                else if (Box2.Text.Length == 0 && e.KeyChar == 8)
+                {
+                    Box1.Focus();
+                    Box1.SelectionStart = Box1.Text.Length;
+                }
+            }
+            else
+                e.Handled = true;
+
+        }
+
+        /// \ifnot hide_events
+        /// Performs KeyPress analysis and handling to ensure a valid ip octet is
+        /// being entered in Box3.
+        /// \endif
+        private void Box3_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            //Identical to Box2_KeyPress except that previous box is Box2 and
+            //next box is Box3
+            if (e.KeyChar.ToString() == "." || Char.IsDigit(e.KeyChar) || e.KeyChar == 8)
+            {
+                if (e.KeyChar.ToString() == ".")
+                {
+                    if (Box3.Text != "" && Box3.SelectionLength != Box3.Text.Length)
+                    {
+                        if (IsValid(Box3.Text, 0, 255))
+                            Box4.Focus();
+                        else
+                            Box3.SelectAll();
+                    }
+                    e.Handled = true;
+                }
+                else if (Box3.SelectionLength != Box3.Text.Length)
+                {
+                    if (Box3.Text.Length == 2)
+                    {
+                        if (e.KeyChar == 8)
+                        {
+                            Box3.Text.Remove(Box3.Text.Length - 1, 1);
+                        }
+                        else if (!IsValid(Box3.Text + e.KeyChar.ToString(), 0, 255))
+                        {
+                            Box3.SelectAll();
+                            e.Handled = true;
+                        }
+                        else
+                        {
+                            Box4.Focus();
+                        }
+                    }
+                }
+                else if (Box3.Text.Length == 0 && e.KeyChar == 8)
+                {
+                    Box2.Focus();
+                    Box2.SelectionStart = Box2.Text.Length;
+                }
+            }
+            else
+                e.Handled = true;
+        }
+
+        /// \ifnot hide_events
+        /// Performs KeyPress analysis and handling to ensure a valid ip octet is
+        /// being entered in Box4.
+        /// \endif
+        private void Box4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Similar to Box3 but ignores the '.' character and does not advance
+            //to the next box.  Also Box3 is previous box for backspace case.
+            if (Char.IsDigit(e.KeyChar) || e.KeyChar == 8)
+            {
+                if (Box4.SelectionLength != Box4.Text.Length)
+                {
+                    if (Box4.Text.Length == 2)
+                    {
+                        if (e.KeyChar == 8)
+                        {
+                            Box4.Text.Remove(Box4.Text.Length - 1, 1);
+                        }
+                        else if (!IsValid(Box4.Text + e.KeyChar.ToString(), 0, 255))
+                        {
+                            Box4.SelectAll();
+                            e.Handled = true;
+                        }
+                    }
+                }
+                else if (Box4.Text.Length == 0 && e.KeyChar == 8)
+                {
+                    Box3.Focus();
+                    Box3.SelectionStart = Box3.Text.Length;
+                }
+            }
+            else
+                e.Handled = true;
+        }
+
+        /// <summary>
+        /// Handles the Enter event of the Box control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        /// \ifnot hide_events
+        /// Performs KeyPress analysis and handling to ensure a valid ip octet is
+        /// being entered in Box4.
+        /// \endif
+        /// \ifnot hide_events
+        /// Selects All text in a box for overwriting upon entering the box
+        /// \endif
+        private void Box_Enter(object sender, System.EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.SelectAll();
+        }
+
+        /// <summary>
+        /// Handles the EnabledChanged event of the label control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        /// \ifnot hide_events
+        /// Ensures a consistent "grayed out" look when the control is disabled
+        /// \endif
+        private void label_EnabledChanged(object sender, System.EventArgs e)
+        {
+            Label lbl = (Label)sender;
+            if (lbl.Enabled)
+                lbl.BackColor = SystemColors.Window;
+            else
+                lbl.BackColor = SystemColors.Control;
+        }
+
+        /// <summary>
+        /// Handles the EnabledChanged event of the panel1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        /// \ifnot hide_events
+        /// Ensures a consistent "grayed out" look when the control is disabled
+        /// \endif
+        private void panel1_EnabledChanged(object sender, System.EventArgs e)
+        {
+            Panel pan = (Panel)sender;
+            if (pan.Enabled)
+                pan.BackColor = SystemColors.Window;
+            else
+                pan.BackColor = SystemColors.Control;
+        }
+    }
+}
